5006330d5e0070f6929eec388986022d71be212a
Taranpreet Singh <taran2994@gmail.com>
2021-03-10 01:06:49 -0500

    Updating Books RecyclerView

--- app/src/main/java/BooksRecyclerViewAdapter.java
+++ app/src/main/java/BooksRecyclerViewAdapter.java
@@ -1,3 +1,5 @@
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,22 +11,42 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mylibraryapp.R;
 
+import java.util.ArrayList;
+
 public class BooksRecyclerViewAdapter extends RecyclerView.Adapter<BooksRecyclerViewAdapter.ViewHolder>  {
+
+    private ArrayList<Book> bookList= new ArrayList<>();
+    private Context cntnxt;
+
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+        notifyDataSetChanged();
+    }
+
+    public BooksRecyclerViewAdapter(Context context)
+    {
+        this.cntnxt=context;
+
+    }
+
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_book,parent,false);
+        ViewHolder holder= new ViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return bookList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
